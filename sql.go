91b0f3c7ff137b64a0d4b8c069c780a8bf14cd58
Joseph Palacio <josephmpalacio@gmail.com>
2021-07-02 17:04:05 -0500

    Revert "refactor and add table package"

--- sql.go
+++ sql.go
@@ -1,6 +1,9 @@
 package sql
 
 import (
+	"fmt"
+	"regexp"
+
 	_ "github.com/golang-migrate/migrate/source/file"
 	"github.com/jmoiron/sqlx"
 )
@@ -25,4 +28,19 @@ func New(in ...Option) (DB, error) {
 	}
 
 	return opts, opts.IsValid()
+}
+
+func ToNamedStatement(dbType, stmt string, names []string) string {
+	var r *regexp.Regexp
+	switch dbType {
+	case postgresSource:
+		r = regexp.MustCompile(`\$\d`)
+	case mysqlSource:
+		r = regexp.MustCompile(`\?`)
+	}
+	var i int
+	return r.ReplaceAllStringFunc(stmt, func(s string) string {
+		defer func() { i++ }()
+		return fmt.Sprintf(":%s", names[i])
+	})
 }
